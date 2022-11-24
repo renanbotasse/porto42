daa7551ffff763387e6c03853d0867e5569187dc
renanbotasse <renanbotasse@gmail.com>
2022-11-24 16:23:15 +0000

    ft_memmove

---
+++ lvl_0_LIBFT/ft_memmove/ft_memmove.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbotasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 16:16:15 by rbotasse          #+#    #+#             */
+/*   Updated: 2022/11/24 16:22:37 by rbotasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	if (!src && !dest)
+		return (NULL);
+	if (src < dest)
+	{
+		while (n != 0)
+		{
+			n--;
+			((char *)dest)[n] = ((char *)src)[n];
+		}
+	}
+	else if (src > dest)
+		ft_memcpy(dest, src, n);
+	return (dest);
+}
