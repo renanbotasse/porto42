3ea08108d21beea79fcc61fb99f7b947060252e1
renanbotasse <renanbotasse@gmail.com>
2022-11-24 15:39:38 +0000

    ft_memset

---
+++ lvl_0_LIBFT/ft_memset/ft_memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbotasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 15:26:11 by rbotasse          #+#    #+#             */
+/*   Updated: 2022/11/24 15:38:07 by rbotasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char *)s;
+	while (i < n)
+	{
+		*ptr = (unsigned char)c;
+		i++;
+		ptr++;
+	}
+	return (s);
+}
