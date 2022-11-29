e85a7abb3ddfb6bb1aa6f2c32c8943bacfdf27fe
renanbotasse <renanbotasse@gmail.com>
2022-11-29 11:24:09 +0000

    ft_strlcat

--- lvl_0_LIBFT/ft_strlcat/ft_strlcat.c
+++ lvl_0_LIBFT/ft_strlcat/ft_strlcat.c
@@ -6,7 +6,31 @@
 /*   By: rbotasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 16:34:39 by rbotasse          #+#    #+#             */
-/*   Updated: 2022/11/24 16:34:40 by rbotasse         ###   ########.fr       */
+/*   Updated: 2022/11/29 11:22:53 by rbotasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	if (destsize < i)
+	{
+		while (src[j])
+			j++;
+		return (destsize + j);
+	}
+	while (destsize > 0 && i < destsize - 1 && src[j])
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	while (src[j++])
+		i++;
+	return (i);
+}
