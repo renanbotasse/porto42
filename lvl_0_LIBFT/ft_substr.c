a338dbe1e003cc6b45194aa614b4ef2b041eb6a1
renanbotasse <renanbotasse@gmail.com>
2022-11-29 21:06:03 +0000

    ft_substr.c

---
+++ lvl_0_LIBFT/ft_substr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbotasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 21:05:39 by rbotasse          #+#    #+#             */
+/*   Updated: 2022/11/29 21:05:41 by rbotasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char				*string;
+	unsigned int		i;
+	unsigned int		a;
+
+	i = start;
+	a = 0;
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+	{
+		string = malloc(1 * sizeof(char));
+		if (string == NULL)
+			return (NULL);
+		string[0] = '\0';
+		return (string);
+	}
+	string = malloc((len + 1) * sizeof(char));
+	if (string == NULL)
+		return (NULL);
+	while (i < ft_strlen(s) && len-- > 0)
+		string[a++] = s[i++];
+	string[a] = '\0';
+	return (string);
+}
