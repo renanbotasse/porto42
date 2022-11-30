967ecd96f90abb0e3a3eb5ab435e8ef264511fd7
renanbotasse <renanbotasse@gmail.com>
2022-11-30 00:00:05 +0000

    ft_strtrim

---
+++ lvl_0_LIBFT/ft_strtrim.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbotasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 21:45:40 by rbotasse          #+#    #+#             */
+/*   Updated: 2022/11/29 21:45:42 by rbotasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n && src[i] == '\0')
+	{
+		while (dest[i] != '\0')
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	beg;
+	size_t	end;
+	char	*trimmed_str;
+
+	if (!s1 || !set)
+		return (NULL);
+	beg = 0;
+	while (s1[beg] != '\0' && ft_strchr(set, s1[beg]) != NULL)
+		beg++;
+	end = ft_strlen (s1 + beg);
+	if (end != '\0')
+	{
+		while (s1[beg + end - 1] != '\0' && ft_strchr(set, s1[beg + end - 1])
+			!= NULL)
+			end--;
+	}
+	trimmed_str = malloc((end + 1) * sizeof(char));
+	if (trimmed_str == NULL)
+		return (NULL);
+	ft_strncpy(trimmed_str, (s1 + beg), end);
+	return (trimmed_str);
+}
