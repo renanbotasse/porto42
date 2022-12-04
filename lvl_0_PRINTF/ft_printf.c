37fe1d627b9e11343098e43e82c7f74fcdd4afc6
renanbotasse <renanbotasse@gmail.com>
2022-12-04 15:40:29 +0000

    ft_printf v3

--- lvl_0_PRINTF/ft_printf.c
+++ lvl_0_PRINTF/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rbotasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 14:28:17 by rbotasse          #+#    #+#             */
-/*   Updated: 2022/12/04 15:39:32 by rbotasse         ###   ########.fr       */
+/*   Updated: 2022/12/04 15:40:16 by rbotasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,23 @@ static int  ft_converter(char cl, va_list arg, int n)
     }
     else if (cl == 'c')
     {
-        ft_putchar_fd(va_arg(arg, int), n);
+        ft_putchar_n(va_arg(arg, int), n);
         return (1)
     }
     else if (cl == 's')
-        return(ft_puts_fd(va_arg(arg, char *), n));
+        return(ft_puts_n(va_arg(arg, char *), n));
     else if (cl == 'p')
-        return(ft_putp_fd(va_arg(arg, unsigned long long), n));
+        return(ft_putp_n(va_arg(arg, unsigned long long), n));
     else if (cl == 'd')
-        return(ft_putd_fd(va_arg(arg, int), n));
+        return(ft_putd_n(va_arg(arg, int), n));
     else if (cl == 'i')
-        return(ft_puti_fd(va_arg(arg, int), n));
+        return(ft_puti_n(va_arg(arg, int), n));
     else if (cl == 'u')
-        return(ft_putu_fd(va_arg(arg, unsigned int), n));
+        return(ft_putu_n(va_arg(arg, unsigned int), n));
     else if (cl == 'x')
-        return(ft_putx_fd(va_arg(arg, unsigned int), n, cl));
+        return(ft_putx_n(va_arg(arg, unsigned int), n, cl));
     else if (cl == 'X')
-        return(ft_putX_fd(va_arg(arg, unsigned int), n, cl));
+        return(ft_putX_n(va_arg(arg, unsigned int), n, cl));
     else
         return(write(1, 'Sorry', 5));
 }
